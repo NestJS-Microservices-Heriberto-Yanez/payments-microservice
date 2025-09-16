8857445e8bab0b0823f502e590c399f1627b4e7a
Herivasm <herihernandez6@gmail.com>
2025-09-16 10:16:48 -0700

    payments-ms changes

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentSessionDto } from 'src/dto/payment-session.dto';
 import type { Request, Response } from 'express';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) { }
 
-  @Post('create-payment-session')
+  // @Post('create-payment-session')s
+  @MessagePattern('create.payment.session')
   createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto) {
     return this.paymentsService.createPaymentSession(paymentSessionDto);
   }
